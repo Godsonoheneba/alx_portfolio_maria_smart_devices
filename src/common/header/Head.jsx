583b6977258a7f8cefc3f9f2c01eaf133cd1f59d
ocheja1 <favourocheja@gmail.com>
2024-01-14 19:29:50 +0100

    missing styling Header Done

---
+++ src/common/header/Head.jsx
@@ -0,0 +1,28 @@
+import React from "react"
+
+const Head = () => {
+  return (
+    <>
+      <section className='head'>
+        <div className='container d_flex'>
+          <div className='left row'>
+            <i className='fa fa-phone'></i>
+            <label> +233548157455 / +233548157455 / +233548157455  </label>
+            <i className='fa fa-envelope'></i>
+            <label> support@maria.com</label>
+          </div>
+          <div className='right row RText'>
+            <label>Theme FAQ"s</label>
+            <label>Need Help?</label>
+            <span>🇬🇭</span> 
+            <label>GH</label>
+            <span>🇬🇭</span> 
+            <label>GH&#8373;</label>
+          </div>
+        </div>
+      </section>
+    </>
+  )
+}
+
+export default Head
