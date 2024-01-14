8fcb42c2275887e071a669492a1fdaff938cf1ba
Godson Oheneba Dacosta <ohenebadac@gmail.com>
2024-01-14 06:29:15 +0000

    Updated discount price unit

--- src/components/discount/Dcard.jsx
+++ src/components/discount/Dcard.jsx
@@ -24,7 +24,7 @@ const Dcard = () => {
                   <img src={value.cover} alt='' width='100%' />
                 </div>
                 <h4>{value.name}</h4>
-                <span>{value.price}</span>
+                <span>GH&#8373;{value.price}</span>
               </div>
             </>
           )
