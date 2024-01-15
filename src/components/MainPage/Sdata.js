eb6538786a19c6f8fdda5623f84849ba2a733258
emmanuelofor <emmanuel.ofori@kibo.school>
2024-01-15 06:53:04 +0000

    updated Sdata.js

--- src/components/MainPage/Sdata.js
+++ src/components/MainPage/Sdata.js
@@ -2,7 +2,7 @@ const Sdata = [
     {
       id: 1,
       title: "50% Off For Your First Shopping",
-      desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quis lobortis consequat eu, quam etiam at quis ut convallis.",
+      desc: "shop early for the best deals.",
       cover: "./images/SlideCard/slide-1.png",
     },
     {
