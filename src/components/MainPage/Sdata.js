421dfaa3698d3df35319d10f128a5254e63c05f2
emmanuelofor <emmanuel.ofori@kibo.school>
2024-01-15 06:33:00 +0000

    updated Mainpage sdata.js

--- src/components/MainPage/Sdata.js
+++ src/components/MainPage/Sdata.js
@@ -8,19 +8,19 @@ const Sdata = [
     {
       id: 2,
       title: "50% Off For Your First Shopping",
-      desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quis lobortis consequat eu, quam etiam at quis ut convallis.",
+      desc: "shop early for the best deals.",
       cover: "./images/SlideCard/slide-2.png",
     },
     {
       id: 3,
       title: "50% Off For Your First Shopping",
-      desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quis lobortis consequat eu, quam etiam at quis ut convallis.",
+      desc: "shop early for the best deals.",
       cover: "./images/SlideCard/slide-3.png",
     },
     {
       id: 4,
       title: "50% Off For Your First Shopping",
-      desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quis lobortis consequat eu, quam etiam at quis ut convallis.",
+      desc: "shop early for the best deals.",
       cover: "./images/SlideCard/slide-4.png",
     },
   ]
